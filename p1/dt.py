f4a8d3ba84f1c3e60a42cc860355306302a99c24
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-10-03 13:23:25 +0200

    create_trees

--- p1/dt.py
+++ p1/dt.py
@@ -17,6 +17,22 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Put your funtions here
 # ...
+def create_trees():
+	dataset1_X, dataset1_y = make_dataset1
+	dataset2_X, dataset2_y = make_dataset2
+
+	decTree_1 = DecisionTreeClassifier(max_depth = 1)
+	decTree_2 = DecisionTreeClassifier(max_depth = 2)
+	decTree_4 = DecisionTreeClassifier(max_depth = 4)
+	decTree_8 = DecisionTreeClassifier(max_depth = 8)
+	decTree_none = DecisionTreeClassifier(max_depth = none)
+
+	decTrees[] = {decTree_1, decTree_2, decTree_4, decTree_8, decTree_none}
+
+	for(decTree in decTrees):
+		decTree.fit(X, y)
+
+	return decTrees
 
 
 if __name__ == "__main__":
