4034072267d6f66666cdd6eec74d0aa93e0d3ff4
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-10-07 22:29:47 +0200

    compute_accuracies

--- p1/dt.py
+++ p1/dt.py
@@ -8,11 +8,12 @@ Project 1 - Classification algorithms
 
 import numpy as np
 from matplotlib import pyplot as plt
-
 from data import make_dataset1, make_dataset2
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from plot import plot_boundary
 import graphviz
 
 
@@ -21,30 +22,60 @@ import graphviz
 
 # Put your funtions here
 # ...
+
+N_POINTS = 1500
+
 def create_trees(X, y):
-	
-    decTree_1 = DecisionTreeClassifier(max_depth = 1)
-    decTree_2 = DecisionTreeClassifier(max_depth = 2)
-    decTree_4 = DecisionTreeClassifier(max_depth = 4)
-    decTree_8 = DecisionTreeClassifier(max_depth = 8)
-    decTree_none = DecisionTreeClassifier(max_depth = None)
 
-    decTrees = [decTree_1, decTree_2, decTree_4, decTree_8, decTree_none]
+    depth = [1, 2, 4, 8, None]
 
-    for decTree in decTrees:
-    	decTree.fit(X, y)
+    decTrees = []
+
+    for i in range(len(depth)):
+        decTree = DecisionTreeClassifier(max_depth = depth[i])
+        decTree.fit(X, y)
+        decTrees.append(decTree)
 
     return decTrees
 
+def makePlot():
+    
+    
+    files = ["decTree_1", "decTree_2", "decTree_4", "dec_Tree_8", "decTree_none"]
+    filesTree = ["tree_" + x for x in files]
+
+    X, y = make_dataset1(N_POINTS)
+
+    decTrees = create_trees(X, y)
+
+    for i in range(len(depth)):
+        plot_boundary(files[i], decTrees[i], X, y) #Il faut mettre les test et pas les train?
+        graph = graphviz.Source(export_graphviz(decTrees[i], out_file = None))
+        graph.render(filesTree[i], view = False)
+
+def compute_accuracies():
+    NB_TEST = 5
+
+    accuracies = [0] * 5
+
+    for i in range(NB_TEST):
+        X, y = make_dataset1(N_POINTS)
+        trainSetX, testSetX, trainSetY, testSetY = train_test_split(X, y, test_size = 0.2)
+
+        decTrees = create_trees(trainSetX, trainSetY)
+        current_accuracy = [accuracy_score(testSetY, decTree.predict(testSetX)) for decTree in decTrees]
+        accuracies = list(map(lambda x,y: x + y, accuracies, current_accuracy))
+
+    accuracies = [x/5 for x in accuracies]
+
+    return accuracies
 
 if __name__ == "__main__":
-    N_POINTS = 1500
 
-    dataset1_X, dataset1_y = make_dataset1(N_POINTS)
-    dataset2_X, dataset2_y = make_dataset2(N_POINTS)
+    accuracies = compute_accuracies()
+    print(accuracies)
+   
+    #graph = graphviz.Source(export_graphviz(decTrees1[0], out_file = None))
+    #graph.render("test", view = True)
 
-    decTrees1 = create_trees(dataset1_X, dataset1_y)
-    decTrees2 = create_trees(dataset2_X, dataset2_y)
 
-    graph = graphviz.Source(export_graphviz(decTrees1[0], out_file = None))
-    graph.render("test", view = True)
