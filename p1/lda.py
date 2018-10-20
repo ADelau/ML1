e9087e5419efdb734da32cd58832c41c73ad02fa
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-10-20 17:31:21 +0200

    lda marche pas

--- p1/lda.py
+++ p1/lda.py
@@ -48,20 +48,38 @@ class LinearDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         classes = list(set(y))
         nb_classes = len(classes)
         samples = [[]]*nb_classes
+        print(samples)
 
+        #samples[i][j][k] correspond to the kth element of the jth sample corresponding to the class classes[i].
         for i in range(len(y)):
             index = classes.index(y[i])
-            samples[index].append(X[i])
+            print(index)
+            (samples[index]).append(X[i])
+            print(samples)
+
+        samples = np.asarray(samples, dtype = np.float)
+        print(samples)
 
 
         means = [None]*nb_classes
         covariances = [None]*nb_classes
         for i in range(len(classes)):
-            means[i] = np.mean(np.array(samples[i]), axis = 0)
-            covariances[i] = np.cov(np.array(samples[i]))
+            means[i] = np.mean(samples[i], axis = 0)
+            covariances[i] = np.cov(samples[i], rowvar = False)
+
+        #using homoscedasticity property
+        covariances = np.asarray(covariances, dtype = np.float)
+        covariance = np.mean(covariances, axis = 0)
+
+        priorProba = []
+
+        totalSamples = len(y)
+        for i in range(len(classes)):
+            priorProba.append(len(samples[i])/totalSamples) #len(samples[i]) = number of samples in class i
 
         print(means)
-        print(covariances)
+        print(covariance)
+        print(priorProba)
 
         return self
 
@@ -110,9 +128,10 @@ if __name__ == "__main__":
     from data import make_dataset2
     from plot import plot_boundary
 
-    N_POINTS = 1500
+    N_POINTS = 10
+    seed = 11
 
-    X, y = make_dataset2(N_POINTS)
+    X, y = make_dataset2(N_POINTS, seed)
 
     lda = LinearDiscriminantAnalysis()
     lda.fit(X, y)
