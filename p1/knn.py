60dcb9f8f9ffed4c1606dac55589b559161d3398
hokolomopo <adrien.minne@outlook.com>
2018-10-31 14:25:24 +0100

    Fix code

--- p1/knn.py
+++ p1/knn.py
@@ -16,9 +16,7 @@ from sklearn.model_selection import cross_val_score
 from statistics import mean
 from sklearn.model_selection import train_test_split
 
-saveDirectory = "D:/OneDrive/Cours/master1/machine/projet1/p1/"
-
-def get_classifier(dataset_x, dataset_y, n_neighbors) :
+def get_classifier(datasetX, datasetY, nNeighbors) :
     """
     Given a learning sanple and a number of neigbours, return a 
     KNeighborsClassifier object fitting this dataset with the given number
@@ -26,9 +24,9 @@ def get_classifier(dataset_x, dataset_y, n_neighbors) :
 
     Arguments:
     ----------
-    - `dataset_x`: the X values of the LS.
-    - `dataset_y`: the Y values of the LS.
-    - `n_neighbors`: the number of neighbours for the KNeighborsClassifier.
+    - `datasetX`: the X values of the LS.
+    - `datasetY`: the Y values of the LS.
+    - `nNeighbors`: the number of neighbours for the KNeighborsClassifier.
 
     Return:
     -------
@@ -36,14 +34,14 @@ def get_classifier(dataset_x, dataset_y, n_neighbors) :
     """
 
     # Create classifier with the given number of neighbours
-    neigh = KNeighborsClassifier(n_neighbors)
+    neigh = KNeighborsClassifier(nNeighbors)
 
     # Fit the model over the learning sample
-    neigh.fit(dataset_x, dataset_y)
+    neigh.fit(datasetX, datasetY)
 
     return neigh
 
-def draw_boundary(classifier, dataset_x, dataset_y, n_neighbors):
+def draw_boundary(classifier, datasetX, datasetY, nNeighbors):
     """
     Given a dataset, and a KNeighborsClassifier already trained with 
     'n_neighbors' neighbours, plot the boundary for the dataset.
@@ -51,26 +49,26 @@ def draw_boundary(classifier, dataset_x, dataset_y, n_neighbors):
     Arguments:
     ----------
     - `classifier`: a KNeighborsClassifier already trained.
-    - `dataset_x`: the X values of the dataset.
-    - `dataset_y`: the Y values of the dataset.
-    - `n_neighbors`: the number of neighbours for the KNeighborsClassifier.
+    - `datasetX`: the X values of the dataset.
+    - `datasetY`: the Y values of the dataset.
+    - `nNeighbors`: the number of neighbours for the KNeighborsClassifier.
     """
 
     #Plot the boundaries for the test samples
-    plot_boundary(saveDirectory + "n_neighbours" + str(n_neighbors), 
-                    classifier, dataset_x, dataset_y,
-                    title="Number of Neighbours = %s" % str(n_neighbors))
+    plot_boundary("n_neighbours" + str(nNeighbors), 
+                    classifier, datasetX, datasetY,
+                    title="Number of Neighbours = %s" % str(nNeighbors))
 
-def cross_validation(dataset_x, dataset_y, n_neigbours, KFlod=10):
+def cross_validation(datasetX, datasetY, nNeighbors, KFlod=10):
     """
     Given a dataset, and a KNeighborsClassifier already trained with 
-    'n_neighbors' neighbours, plot the boundary for the dataset.
+    'nNeighbors' neighbours, plot the boundary for the dataset.
 
     Arguments:
     ----------
-    - `dataset_x`: the X values of the dataset.
-    - `dataset_y`: the Y values of the dataset.
-    - `n_neighbors`: the number of neighbours for the KNeighborsClassifier.
+    - `datasetX`: the X values of the dataset.
+    - `datasetY`: the Y values of the dataset.
+    - `nNeighbors`: the number of neighbours for the KNeighborsClassifier.
     - `KFlod`: the number of fold in the cross validation.
 
     Return:
@@ -78,10 +76,10 @@ def cross_validation(dataset_x, dataset_y, n_neigbours, KFlod=10):
     - The score of the cross validation.
     """
     # Create classifier with the given number of neighbours
-    neigh = KNeighborsClassifier(number)
+    neigh = KNeighborsClassifier(nNeighbors)
 
     # Compute the cross-validation score of our data
-    crossScore = cross_val_score(neigh, dataset_x, dataset_y,
+    crossScore = cross_val_score(neigh, datasetX, datasetY,
                                  cv=KFlod, n_jobs=-1)
 
     return mean(crossScore)
@@ -89,17 +87,19 @@ def cross_validation(dataset_x, dataset_y, n_neigbours, KFlod=10):
 if __name__ == "__main__":
     seed = 687
 
-    dataset_x, dataset_y = make_dataset2(1500, seed)
-    n_neighbors = [1, 5, 25, 125, 625, 1200]
+    datasetX, datasetY = make_dataset2(1500, seed)
+    nNeighbors = [1, 5, 25, 125, 625, 1200]
 
     LS_size = 1200
 
-    X_train, X_test, y_train, y_test = \
-        train_test_split(dataset_x, dataset_y, test_size = 0.2,random_state = seed)
+    # Split the dataset in Traning and Learning set
+    xTrain, xTest, yTrain, yTest = \
+        train_test_split(datasetX, datasetY, test_size = 0.2,random_state = seed)
+
     # Question 1
-    for number in n_neighbors:
-        classifier = get_classifier(dataset_x[0:LS_size], dataset_y[0:LS_size], number)
-        draw_boundary(classifier, dataset_x[LS_size + 1:], dataset_y[LS_size + 1:], number)
+    for number in nNeighbors:
+        classifier = get_classifier(xTrain, yTrain, number)
+        draw_boundary(classifier, xTest, yTest, number)
 
     # Question 2
     scores = [[], []]
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     for number in range(1,1200,step) :
 
         # Get the cross validation score
-        crossScore = cross_validation(dataset_x, dataset_y, number)
+        crossScore = cross_validation(datasetX, datasetY, number)
         
         # Add this score to the array
         scores[0].append(number)
@@ -120,6 +120,5 @@ if __name__ == "__main__":
     
     plt.figure()
     plt.plot(scores[0], scores[1])
-    plt.savefig(saveDirectory + "scores.pdf")
-    plt.show()
+    plt.savefig("scores.pdf")
     plt.close()
