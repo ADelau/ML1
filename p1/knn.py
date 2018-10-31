174c7436216835c2aac5d3d057ec54ff560fa812
hokolomopo <adrien.minne@outlook.com>
2018-10-31 12:38:03 +0100

    Apply final fixes

--- p1/knn.py
+++ p1/knn.py
@@ -14,10 +14,11 @@ from plot import plot_boundary
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from statistics import mean
+from sklearn.model_selection import train_test_split
 
 saveDirectory = "D:/OneDrive/Cours/master1/machine/projet1/p1/"
 
-def getClassifier(dataset_x, dataset_y, n_neighbors) :
+def get_classifier(dataset_x, dataset_y, n_neighbors) :
     """
     Given a learning sanple and a number of neigbours, return a 
     KNeighborsClassifier object fitting this dataset with the given number
@@ -60,7 +61,7 @@ def draw_boundary(classifier, dataset_x, dataset_y, n_neighbors):
                     classifier, dataset_x, dataset_y,
                     title="Number of Neighbours = %s" % str(n_neighbors))
 
-def crossValidation(dataset_x, dataset_y, n_neigbours, KFlod=10):
+def cross_validation(dataset_x, dataset_y, n_neigbours, KFlod=10):
     """
     Given a dataset, and a KNeighborsClassifier already trained with 
     'n_neighbors' neighbours, plot the boundary for the dataset.
@@ -86,24 +87,29 @@ def crossValidation(dataset_x, dataset_y, n_neigbours, KFlod=10):
     return mean(crossScore)
 
 if __name__ == "__main__":
-    dataset_x, dataset_y = make_dataset2(1500, 687)
+    seed = 687
+
+    dataset_x, dataset_y = make_dataset2(1500, seed)
     n_neighbors = [1, 5, 25, 125, 625, 1200]
 
+    LS_size = 1200
+
+    X_train, X_test, y_train, y_test = \
+        train_test_split(dataset_x, dataset_y, test_size = 0.2,random_state = seed)
     # Question 1
-    # LS_size = 1200
-    # for number in n_neighbors:
-    #     classifier = getClassifier(dataset_x[0:LS_size], dataset_y[0:LS_size], number)
-    #     draw_boundary(classifier, dataset_x[LS_size + 1:], dataset_y[LS_size + 1:], number)
+    for number in n_neighbors:
+        classifier = get_classifier(dataset_x[0:LS_size], dataset_y[0:LS_size], number)
+        draw_boundary(classifier, dataset_x[LS_size + 1:], dataset_y[LS_size + 1:], number)
 
     # Question 2
     scores = [[], []]
-    step = 30
+    step = 100
 
     # Loop over the number of neighbours used to the classifier
     for number in range(1,1200,step) :
 
         # Get the cross validation score
-        crossScore = crossValidation(dataset_x, dataset_y, number)
+        crossScore = cross_validation(dataset_x, dataset_y, number)
         
         # Add this score to the array
         scores[0].append(number)
@@ -117,5 +123,3 @@ if __name__ == "__main__":
     plt.savefig(saveDirectory + "scores.pdf")
     plt.show()
     plt.close()
-
-    # validation(dataset_x, dataset_y, n_neighbors)
